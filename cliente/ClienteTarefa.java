b4553bdd2f772be8b55b0ee2578121affeeaab81
jonassantos1000 <jonasvale3@hotmail.com>
2022-05-20 12:45:07 -0300

    Ajustado para o cliente receber e enviar comandos para o servidor

--- cliente/ClienteTarefa.java
+++ cliente/ClienteTarefa.java
@@ -1,5 +1,6 @@
 package cliente;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
@@ -8,28 +9,61 @@ public class ClienteTarefa {
 	public static void main(String[] args) throws Exception {
 		Socket socket = new Socket ("localhost", 12345);
 		System.out.println("Conexao estabelecida");
-		PrintStream saida = new PrintStream(socket.getOutputStream());
+		
+		Thread enviaComando = new Thread(new Runnable() {
 
+			@Override
+			public void run() {
+				try {
+					System.out.println("Enviar comando para o server");
+					PrintStream saida = new PrintStream(socket.getOutputStream());
+					Scanner teclado = new Scanner(System.in);
+					while(teclado.hasNextLine()) {
+						
+						String comando = teclado.nextLine();
+						
+						if(comando.trim().equals("")) {
+							break;
+						}
+						
+						saida.println(comando);
+					}
+					
+					saida.close();
+					teclado.close();
+					
+				}catch(IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
 		
-		Scanner teclado = new Scanner(System.in);
-		while(teclado.hasNextLine()) {
-			String comando = teclado.nextLine();
-			if(comando.trim().equals("")) {
-				break;
-			};
-			saida.println(comando);
-		}
+		Thread recebeComando = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					System.out.println("Recebendo info do server");
+					Scanner respostaServidor = new Scanner(socket.getInputStream());
+					while(respostaServidor.hasNextLine()) {
+						String resposta = respostaServidor.nextLine();
+						System.out.println(resposta);
+					}
+					respostaServidor.close();
+					
+				} catch(IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
 		
-		Scanner respostaServidor = new Scanner(socket.getInputStream());
+		recebeComando.start();
+		enviaComando.start();
 		
-		while(respostaServidor.hasNextLine()) {
-			String resposta = respostaServidor.nextLine();
-			System.out.println(resposta);
-		}
-		respostaServidor.close();
+		enviaComando.join();
 		
-		saida.close();
-		teclado.close();
+		System.out.println("Finalizando conexão");
+		
 		socket.close();
 	}
 }
